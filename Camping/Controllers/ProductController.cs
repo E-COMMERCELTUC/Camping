50b220ee6350f0b141ee571e925b2418969d4d73
Nadine  Aleideh <nadinealeideh75@gmail.com>
2023-08-29 20:54:35 +0300

    Admin view list of product, and each product alone is done

--- Camping/Controllers/ProductController.cs
+++ Camping/Controllers/ProductController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Camping.Models;
+using Camping.Models.DTO;
+using Camping.Models.Interface;
+using Camping.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Camping.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+
+        private readonly IProduct _context;
+
+        public ProductController(IProduct product)
         {
-            return View();
+            _context = product;
         }
+        public async Task<IActionResult> ProductGetAll()
+        {
+            List<ProductDTO> products = await _context.GetAll();
+            return View(products);
+        }
+
+        public async Task<IActionResult> ProductDetails(int id)
+        {
+            var products = await _context.GetById(id);
+            return View(products);
+        }
+
     }
 }
--- Camping/Models/DTO/ProductDTO.cs
+++ Camping/Models/DTO/ProductDTO.cs
@@ -1,6 +1,18 @@
-﻿namespace Camping.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Camping.Models.DTO
 {
     public class ProductDTO
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public string Spec { get; set; }
+
+        public int CategoryID { get; set; }
+
+        [Display(Name = "Category")]
+        public string ProductCategoryName { get; set; }
+        public Category Categories { get; set; }
     }
 }
